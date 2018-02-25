bb0f7240b458465b78eab3bdd41829f25318b948
harshbafna <ravindra_joshi@persistent.co.in>
2018-02-25 12:02:13 +0530

    check if .pem file exists.

--- clone_shareinsights.sh
+++ clone_shareinsights.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# make sure that the path of .pem file is passed as first argument. 
+if [ $# == 0 ]; then
+  echo "Please pass the path of .pem file as first argument."
+  exit 1
+else
+  if [ -e $1 ];
+  then 
+	  echo "File \"$(basename $1)\" exists, moving ahead ...."
+  else
+	  echo "File \"$(basename $1)\" does not exist, please specify the correct path."
+    exit 1
+  fi
+fi
+
 # TODO: Remove the host entry from hosts file. 
 
 sudo mkdir /opt/workspace
